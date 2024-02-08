e3083ee651781cd4d6f5be42af8c66d08d1c8921
StasSemak <semaks051@gmail.com>
2024-02-08 13:04:19 +0200

    feat: optimize loops, make func

--- task6/main.go
+++ task6/main.go
@@ -6,12 +6,23 @@ import (
 )
 
 func printMatrix(m [][]float64) {
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m[0]); j++ {
+	for i := range m {
+		for j := range m[0] {
 			fmt.Printf("%.2f\t", m[i][j])
 		}
 		fmt.Println()
 	}
+}
+
+func makeMatrix(x, y int) [][]float64 {
+	m := make([][]float64, x)
+	for i := range m {
+		m[i] = make([]float64, y)
+	}
+	return m
+}
+func makeRowMatrix(x int) []float64 {
+	return make([]float64, x)
 }
 
 func det(m [][]float64) float64 {
@@ -26,17 +37,42 @@ func det(m [][]float64) float64 {
 	return m[0][0]*m[1][1] - m[0][1]*m[1][0]
 }
 
+func multiplyRow(a [][]float64, b []float64) []float64 {
+	m := makeRowMatrix(len(a))
+
+	for i := range m {
+		for j := range m {
+			m[i] += a[i][j] * b[j]
+		}
+	}
+
+	return m
+}
+
+func reverse(m [][]float64) [][]float64 {
+	d := det(m)
+	mRev := makeMatrix(len(m), len(m[0]))
+
+	for i := range m {
+		for j := range m[0] {
+			mRev[j][i] = (math.Pow(-1, float64(i+j+2)) * det(subMatrix(m, i, j))) / d
+		}
+	}
+
+	return mRev
+}
+
 func subMatrix(m [][]float64, iSkip int, jSkip int) [][]float64 {
-	sm := [][]float64{{0, 0}, {0, 0}}
+	sm := makeMatrix(len(m)-1, len(m[0])-1)
 	iAct, jAct := 0, 0
 
-	for i := 0; i < len(m); i++ {
+	for i := range m {
 		if i == iSkip {
 			continue
 		}
 
 		jAct = 0
-		for j := 0; j < len(m[0]); j++ {
+		for j := range m[0] {
 			if j == jSkip {
 				continue
 			}
@@ -49,63 +85,30 @@ func subMatrix(m [][]float64, iSkip int, jSkip int) [][]float64 {
 
 	return sm
 }
-func multiplyAB(a [][]float64, b []float64) []float64 {
-	m := []float64{0, 0, 0}
-
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m); j++ {
-			m[i] += a[i][j] * b[j]
-		}
-	}
-
-	return m
-}
-func reverse(m [][]float64) [][]float64 {
-	d := det(m)
-	mRev := [][]float64{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
-
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			mRev[j][i] = (math.Pow(-1, float64(i+j+2)) * det(subMatrix(m, i, j))) / d
-		}
-	}
-
-	return mRev
-}
 
 func copy(src [][]float64, dest *[][]float64) {
-	for i := 0; i < len(src); i++ {
-		for j := 0; j < len(src[0]); j++ {
+	for i := range src {
+		for j := range src[0] {
 			(*dest)[i][j] = src[i][j]
 		}
 	}
 }
 func replaceIWithB(a [][]float64, i int, b []float64) [][]float64 {
-	m := [][]float64{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+	m := makeMatrix(len(a), len(a[0]))
 	copy(a, &m)
 
-	for j := 0; j < len(a[0]); j++ {
+	for j := range m {
 		m[j][i] = b[j]
 	}
 
 	return m
 }
-func kramer(a [][]float64, b []float64) []float64 {
-	x := []float64{0, 0, 0}
-	detA := det(a)
-
-	for i := 0; i < len(x); i++ {
-		x[i] = det(replaceIWithB(a, i, b)) / detA
-	}
-
-	return x
-}
 
 func transpile(m [][]float64) [][]float64 {
-	mt := [][]float64{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+	mt := makeMatrix(len(m), len(m))
 
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m[0]); j++ {
+	for i := range mt {
+		for j := range mt[i] {
 			mt[j][i] = m[i][j]
 		}
 	}
@@ -113,10 +116,7 @@ func transpile(m [][]float64) [][]float64 {
 	return mt
 }
 func multiply(a, b [][]float64) [][]float64 {
-	m := make([][]float64, len(a))
-	for i := range m {
-		m[i] = make([]float64, len(b[0]))
-	}
+	m := makeMatrix(len(a), len(b[0]))
 
 	for i := range m {
 		for j := range m[i] {
@@ -129,10 +129,25 @@ func multiply(a, b [][]float64) [][]float64 {
 	return m
 }
 
+func reversedMatrix(a [][]float64, b []float64) []float64 {
+	return multiplyRow(reverse(a), b)
+}
+
+func kramer(a [][]float64, b []float64) []float64 {
+	x := makeRowMatrix(len(b))
+	detA := det(a)
+
+	for i := range x {
+		x[i] = det(replaceIWithB(a, i, b)) / detA
+	}
+
+	return x
+}
+
 func smallestSquare(a [][]float64, b []float64) []float64 {
 	at := transpile(a)
 
-	return multiplyAB(reverse(multiply(at, a)), multiplyAB(at, b))
+	return multiplyRow(reverse(multiply(at, a)), multiplyRow(at, b))
 }
 
 func main() {
@@ -144,7 +159,7 @@ func main() {
 	b := []float64{-10, 10, 3}
 
 	//method of reversed matrix
-	xRev := multiplyAB(reverse(a), b)
+	xRev := reversedMatrix(a, b)
 	printMatrix([][]float64{xRev})
 
 	//kramer method
